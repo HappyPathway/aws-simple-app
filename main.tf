c8a0b39fceaee1b02442963c75e8da1acc4d6491
Dave Arnold <darnold@hashicorp.com>
2018-06-19 20:13:24 -0700

    bumping version of private-service

--- main.tf
+++ main.tf
@@ -8,11 +8,13 @@ data "terraform_remote_state" "network" {
 
 module "simple-app" {
   source              = "app.terraform.io/Darnold-Hashicorp/private-service/aws"
-  version             = "1.0.2"
+  version             = "1.0.3"
   env                 = "${var.env}"
   key_name            = "${data.terraform_remote_state.network.key_name}"
   private_subnet_id   = "${data.terraform_remote_state.network.private_subnet}"
   service_healthcheck = "add/1/1"
   service_name        = "simple-app"
   service_version     = "1.0.0"
+  instance_type       = "m4.large"
+  vpc_id              = "${data.terraform_remote_state.network.vpc_id}"
 }
