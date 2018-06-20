4e6ff3ea4040d37499b8af78d50ac32fb9d21275
Dave Arnold <darnold@hashicorp.com>
2018-06-19 22:04:33 -0700

    bumping version number and adding packer.json config to create AMIs that will work with this setup

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "simple-app" {
   private_subnet_id   = "${data.terraform_remote_state.network.private_subnet}"
   service_healthcheck = "add/1/1"
   service_name        = "simple-app"
-  service_version     = "1.0.0"
+  service_version     = "1.0.1"
   instance_type       = "m4.large"
   vpc_id              = "${data.terraform_remote_state.network.vpc_id}"
 }
