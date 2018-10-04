69b369f4b9e574168e7b4676fd075fd907f72d23
David Arnold <dja+djaboxx@sonic.net>
2018-10-04 09:19:41 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "simple-app" {
   version             = "1.1.10"
   env                 = "${var.env}"
   key_name            = "${data.terraform_remote_state.network.key_name}"
-  private_subnet_id   = "${data.terraform_remote_state.network.private_subnet}"
+  private_subnet_id   = "${element(data.terraform_remote_state.network.private_subnets, 0)}"
   service_healthcheck = "${var.service_healthcheck}"
   service_name        = "${var.service_name}"
   service_version     = "${var.service_version}"
